2b82065d593d352a08b0ea09b652325f5034e2e0
Joe Fairburn <joe.fairburn1@gmail.com>
2024-11-10 23:10:53 +0000

    docs: add progress demo

---
+++ docs/src/content/docs/guides/quickstart/components/WaveformWithProgress.tsx
@@ -0,0 +1,27 @@
+import Wave from 'wavo';
+import { dataPoints } from '@docs/fixtures/data';
+import { useState } from 'react';
+
+const WaveformWithProgress = () => {
+  const [progress, setProgress] = useState(0);
+
+  const handleProgressChange = (value: number) => {
+    setProgress(value);
+  };
+
+  return (
+    <Wave.Container
+      className="w-full text-neutral-400"
+      dataPoints={dataPoints}
+      gap={1}
+      width={2}
+      onClick={handleProgressChange}
+      onDrag={handleProgressChange}
+    >
+      <Wave.Bars gap={1} width={2} />
+      <Wave.Progress progress={progress} color="#FFF" />
+    </Wave.Container>
+  );
+};
+
+export default WaveformWithProgress;
