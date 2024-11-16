98bb8c3172004ed752fef6978122e54698dd66db
Joe Fairburn <joe.fairburn1@gmail.com>
2024-11-16 16:08:11 +0000

    docs: add more examples

---
+++ docs/src/content/docs/components/WavoExample.tsx
@@ -0,0 +1,69 @@
+import { useRef, useState } from 'react';
+import Waveform from 'wavo';
+import { musicFile, dataPoints } from '@docs/fixtures/data';
+
+const WavoExample = () => {
+  const [progress, setProgress] = useState(0);
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const handleTimeUpdate = () => {
+    if (audioRef.current) {
+      const percentage = audioRef.current.currentTime / audioRef.current.duration;
+      setProgress(percentage);
+    }
+  };
+
+  const handleClick = (percentage: number) => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = audioRef.current.duration * percentage;
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<SVGSVGElement>) => {
+    const STEP = 0.05; // 5% increment/decrement
+
+    switch (event.key) {
+      case 'ArrowLeft':
+        handleClick(Math.max(0, progress - STEP));
+        event.preventDefault();
+        break;
+      case 'ArrowRight':
+        handleClick(Math.min(1, progress + STEP));
+        event.preventDefault();
+        break;
+      case ' ':
+        event.preventDefault();
+        if (audioRef.current?.paused) {
+          audioRef.current.play();
+        } else {
+          audioRef.current?.pause();
+        }
+        break;
+    }
+  };
+
+  return (
+    <div
+      className="h-24 w-full flex flex-row items-center justify-center gap-4 p-4"
+      style={{ '--wavo-progress-color': '#f23d75' } as React.CSSProperties}
+    >
+      <audio ref={audioRef} controls className="hidden" src={musicFile} onTimeUpdate={handleTimeUpdate} />
+      <Waveform.Container
+        className="w-full h-full focus:outline-none focus-visible:ring-1 focus-visible:ring-red-300 focus-visible:ring-opacity-75 rounded-lg"
+        dataPoints={dataPoints}
+        lazyLoad={true}
+        progress={progress}
+        onClick={handleClick}
+        onDrag={handleClick}
+        onDragStart={() => audioRef.current?.pause()}
+        onDragEnd={() => audioRef.current?.play()}
+        onKeyDown={handleKeyDown}
+      >
+        <Waveform.Bars width={2} gap={2} />
+        <Waveform.Progress color="var(--wavo-progress-color)" progress={progress} />
+      </Waveform.Container>
+    </div>
+  );
+};
+
+export default WavoExample;
--- docs/src/content/docs/guides/quickstart/components/WaveformWithProgress.tsx
+++ docs/src/content/docs/guides/quickstart/components/WaveformWithProgress.tsx
@@ -1,6 +1,6 @@
 import Wave from 'wavo';
 import { dataPoints } from '@docs/fixtures/data';
-import { useState } from 'react';
+import { useState, type KeyboardEventHandler } from 'react';
 
 const WaveformWithProgress = () => {
   const [progress, setProgress] = useState(0);
@@ -9,12 +9,30 @@ const WaveformWithProgress = () => {
     setProgress(value);
   };
 
+  // Handle keyboard interaction
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    switch (event.key) {
+      case 'ArrowRight':
+        event.preventDefault();
+        setProgress(progress + 0.02);
+        break;
+      case 'ArrowLeft':
+        event.preventDefault();
+        setProgress(progress - 0.02);
+        break;
+      case ' ':
+        event.preventDefault();
+        break;
+    }
+  };
+
   return (
     <Wave.Container
       className="w-full text-neutral-400"
       dataPoints={dataPoints}
       onClick={handleProgressChange}
       onDrag={handleProgressChange}
+      onKeyDown={handleKeyDown}
     >
       <Wave.Bars gap={1} width={2} />
       <Wave.Progress progress={progress} color="#FFF" />
